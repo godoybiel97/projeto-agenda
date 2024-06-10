e14c206466ba98e1883d9554096b06b2b2d06993
Gabriel Godoy <godoybiel97@gmail.com>
2024-06-09 23:17:01 -0300

    Inserindo arquivos restantes e novas diretivas do Angular 17

---
+++ src/app/componentes/separador/separador.component.ts
@@ -0,0 +1,14 @@
+import { CommonModule, NgFor } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-separador',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './separador.component.html',
+  styleUrl: './separador.component.css'
+})
+
+export class SeparadorComponent {
+  
+}
