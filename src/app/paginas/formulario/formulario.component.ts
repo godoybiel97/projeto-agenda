b85241c9f25ec18e7d43f44755ded1addca9dc43
Gabriel Godoy <godoybiel97@gmail.com>
2024-06-13 12:29:10 -0300

    Estruturação do formulário completa

--- src/app/paginas/formulario/formulario.component.ts
+++ src/app/paginas/formulario/formulario.component.ts
@@ -1,14 +1,33 @@
 import { Component } from '@angular/core';
 import { ContainerComponent } from '../../componentes/container/container.component';
 import { SeparadorComponent } from '../../componentes/separador/separador.component';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-formulario',
   standalone: true,
-  imports: [ContainerComponent, SeparadorComponent],
+  imports: [ContainerComponent, SeparadorComponent, ReactiveFormsModule, NgClass],
   templateUrl: './formulario.component.html',
   styleUrl: './formulario.component.css'
 })
-export class FormularioComponent {
 
+export class FormularioComponent {
+  contatoForm!: FormGroup;
+
+  constructor() {
+    this.contatoForm = new FormGroup({
+      nome: new FormControl('', [Validators.required, Validators.minLength(3)]),
+      telefone: new FormControl('', [Validators.required, Validators.minLength(11)]),
+      email: new FormControl('', [Validators.required, Validators.email]),
+      aniversario: new FormControl(''),
+      redes: new FormControl(''),
+      observacoes: new FormControl('')
+    })
+  }
+
+  onSubmit() {
+    if(this.contatoForm.invalid)
+      console.log(this.contatoForm.get('nome')?.errors)
+    }
 }
